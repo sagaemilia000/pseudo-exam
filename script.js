978f0eec5ea5695e463d5a75e3723b9d2780c405
Saga Eriksson <sagaemilia000@gmail.com>
2024-12-17 13:24:05 +0100

    Finished first part

---
+++ script.js
@@ -0,0 +1,48 @@
+// G-Version - Split the Nota
+// Skriv din pseudokod innanför nedanstående kommentarsblock
+/*
+
+START
+
+SET variabel summa till Totalsumman av notan
+SET variabel antalPersoner till Antal personer i sällskapet
+SET variabel dricks till Dricks i decimalform
+
+IF summa < 0 eller antalPersoner < 1 eller dricks < 0 THEN
+    PRINT "Ogiltiga värden. Kontrollera att dina inmatningar är korrekta."
+END
+
+SET variabel drickssumma till summa * dricks (beräknar dricksen)
+SET variabel totalsumma till summa + drickssumma (beräknar totalsumman med dricks)
+SET variabel beloppPerPerson till totalSumma / antalPersoner (beräknar belopp per person)
+
+PRINT "Varje person ska betala: ", beloppPerPerson
+
+END
+
+*/
+
+// VG-Version - Lewis Carroll Word Puzzle
+// Skriv din pseudokod innanför nedanstående kommentarsblock
+// Startkoden är 100% frivillig och kan tas bort eller skrivas om
+/*
+
+play();
+
+function play()
+    SET variabel ordbok = [FOUR, FOUL, FOOL, FOOT, FORT, FORE, FIRE, FIVE]; // Innehåller ALLA ord i det engelska språket
+    SET variabel startOrd till "FOUR";
+    SET variabel slutordOrd till "FIVE";
+
+
+end function
+
+function isOneLetterApart(wordOne, wordTwo)
+    SET variabel diffCount till 0;
+
+    // Här behöver du skriva koden för din funktion
+
+    return diffCount === 1; // returnerar sant om endast en bokstav ändrats, annars falskt
+end function
+
+*/
